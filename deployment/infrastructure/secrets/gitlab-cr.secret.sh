8bce66ced1783ecb9f0a4d9bc7eda2e4bd5ffb33
radichenko <115370873+radichenko@users.noreply.github.com>
2025-06-03 21:05:02 +0300

    Clone app repository

--- deployment/infrastructure/secrets/gitlab-cr.secret.sh
+++
@@ -1,6 +0,0 @@
-kubectl create secret docker-registry gitlab-registry-secret \
---docker-server=registry.gitlab.com \
---docker-username=kpi-demo-k8s-1 \
---docker-password=glpat-TyzDAx1reg3xPgMgzSe5 \
---docker-email=zotkin.kv@pm.me \
---namespace=default
